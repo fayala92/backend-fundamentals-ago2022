610a0cb66bd61bd309bc003056f4d15bdc4dd698
fayala92 <facundoayala92@gmail.com>
2022-09-06 22:46:51 -0300

    JSON y forms

---
+++ bookingApp/helpers/formatDateTime.js
@@ -0,0 +1,7 @@
+const formatDateTime = (string)=>{
+    const date = new Date(string)
+    const offset = date.getTimezoneOffset()*60000
+    return new Date(date.getTime()-offset).toISOString().slice(0,19).replace('T',' ')
+}
+
+module.exports = formatDateTime
